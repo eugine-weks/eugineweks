2dae8c74d213ecf88057ccd476d7d6d31db118b3
Eugine Weks <neuroweks@gmail.com>
2026-09-19 12:39:11 +0300

    Remove DOMContentLoaded event listener from intApiAnimation

--- js/main.js
+++ js/main.js
@@ -167,7 +167,6 @@ function initContactForm() {
 
 /* -----AI&API animation -------- */
 function intApiAnimation(){
-document.addEventListener("DOMContentLoaded", () => {
       const skillItems = document.querySelectorAll(".skill-item");
       const circumference = 314.16;
 
@@ -201,7 +200,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const observer = new IntersectionObserver(animateSkills, { threshold: 0.1 });
       skillItems.forEach(item => observer.observe(item));
-    });
 }
 
 /* ---------- Boot ---------- */
